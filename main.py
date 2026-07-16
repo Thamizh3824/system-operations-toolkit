eb1695e467da0a6acf966f25fefac8a514920d0b
Thamizh <tthamizharasan209@gmail.com>
2026-07-16 15:20:04 +0530

    Convert application into CLI using argparse

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
+import argparse
+
 from modules.log_analyzer import LogAnalyzer
 
 
 def main() -> None:
+
+    parser = argparse.ArgumentParser(
+        description="System Operations Toolkit"
+    )
+
+    parser.add_argument(
+        "command",
+        choices=[
+            "summary",
+            "export",
+            "filter",
+        ],
+        help="Command to execute"
+    )
+
+    parser.add_argument(
+        "level",
+        nargs="?",
+        help="Log level for filter command"
+    )
+
+    args = parser.parse_args()
+
     analyzer = LogAnalyzer("logs/sample.log")
 
-    analyzer.display_summary()
+    if args.command == "summary":
+        analyzer.display_summary()
 
-    analyzer.export_csv()
+    elif args.command == "export":
+        analyzer.export_csv()
 
-    print("\nERROR Logs\n")
+    elif args.command == "filter":
 
-    error_logs = analyzer.filter_logs("ERROR")
+        if not args.level:
+            print("Please provide a log level.")
+            return
 
-    for log in error_logs:
-        print(
-            f"{log['timestamp']} | "
-            f"{log['level']} | "
-            f"{log['message']}"
-        )
+        logs = analyzer.filter_logs(args.level)
+
+        print(f"\n{args.level.upper()} Logs\n")
+
+        for log in logs:
+            print(
+                f"{log['timestamp']} | "
+                f"{log['level']} | "
+                f"{log['message']}"
+            )
 
 
 if __name__ == "__main__":
