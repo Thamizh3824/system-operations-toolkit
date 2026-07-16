d11354c7ee5d664ac2758a98f4fc66ada70c27f9
Thamizh <tthamizharasan209@gmail.com>
2026-07-16 14:59:12 +0530

    Implement log file reader

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from modules.log_analyzer import LogAnalyzer
 
 def main() -> None:
     analyzer = LogAnalyzer("logs/sample.log")
-    analyzer.validate_file()
-    print("Log file found.")
+
+    logs = analyzer.read_log()
+
+    print(f"Successfully loaded {len(logs)} log entries.\n")
+
+    for log in logs:
+        print(log)
 
 
 if __name__ == "__main__":
--- modules/log_analyzer.py
+++ modules/log_analyzer.py
@@ -8,6 +8,7 @@ class LogAnalyzer:
 
     def __init__(self, log_path: str) -> None:
         self.log_path = Path(log_path)
+        self.log_entries: list[str] = []
 
     def validate_file(self) -> bool:
         """
@@ -26,3 +27,22 @@ class LogAnalyzer:
             )
 
         return True
+    
+    def read_log(self) -> list[str]:
+        """
+        Read all log entries from the log file.
+
+        Returns:
+            list[str]: List of log entries.
+        """
+
+        self.validate_file()
+
+        with self.log_path.open("r", encoding="utf-8") as file:
+            self.log_entries = [
+                line.strip()
+                for line in file
+                if line.strip()
+            ]
+
+        return self.log_entries
